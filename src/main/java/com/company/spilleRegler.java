76eb67945198c8f89f76d5b363c23a850f2e4a1e
Your Name.com <you@example.com>
2019-11-05 12:26:09 +0100

    regler tilføjet i sin egen klasse

---
+++ src/main/java/com/company/spilleRegler.java
@@ -0,0 +1,36 @@
+/*
+package com.company;
+/*
+public class spilleRegelr {
+}
+Før spillet begynder:
+- Der skal være muligheden for at vælge mellem 2-4 spillere.
+- Der skal vælges et spilekort (billedkort) som er din brik gennem spilet. dette er enten en "hund" "kat" "bil" eller "båd".
+- indtast din alder. (her kan det være smart at have grænser på, så man ikke bliver "minus"- år gammel eller over 125)
+- Hvis der er 2 spillere: giv hver spiller 20 Monopolypenge.
+- Hvis der er 3 spillere: giv hver spiller 18 Monopolypenge.
+- Hvis der er 4 spillere: giv hver spiller 16 Monopolypenge.
+- Yngste spiller starter. (hvis spillerne er lige gamle, starter den spiller der indtastede information først.
+- en spiller bliver valgt til bank (ved ikke om dette rent faktisk skal gøres).
+under spil:
+- spiller x kaster med terninger, og rykker det antal pladser øjnene har vist. (med uret, men jeg tror GUI'en gør det for os)
+- hver gang start forbipasseres, modtages 2 Monopolypenge.
+
+Felter og deres funktioner:
+- ET ledigt felt. SKAL købes hvis du lansder på det. der bliver betalt til banken.
+- Et ejet felt: er et felt allerede ejet ,skal deer betales til den spiller der ejer feltet.
+betalingen er dét beløb der står på feltet. er det selvejet skal der ikke ske noget.
+- Dobbelt ejendom: alle ejendomme på brættet er farveinddelt. hvis en spiller har to
+felter med samme farve, skal der betales dobbelt beløb af, hvad der står på feltet.
+dette gælder selvfølgelig for begge ejede felter.
+
+Vind:
+- hvis en spiller ikke kan købe ejendom, betale husleje eller betale afgiften på et chancekort,
+er man gået fallit.
+når dette sker, skal de resterende spillere tælle deres penge, og se hvem der har flest.
+Den med flest penge har vundet. hvis den er uafgjort, tælles ejendommenes værdi,
+og denne tillægges ens pengebeholdning.
+
+yderligere regler findes på: (mangler kun brættets fælter og forklaring dertil).
+https://drive.google.com/file/d/1FS9D8KlX5HGwTusJ6-hXAn1rntCrX_jv/view
+*/
